66ebc5b51219da490d11cb631f7545e55dd3d30e
Saul <jermypreciado@gmail.com>
2023-12-18 00:38:39 -0500

    gh repo clone Sual20/algorit

---
+++ Transforme  Unidades.py
@@ -0,0 +1,4 @@
+def convertidor_unidadades():
+    print("Welcome este es el conversor de unidades metricas.")
+    valor = float(input("Ingresa un valor numerico:"))
+    unidad_origen = (input("igresa un valor numerico"))
